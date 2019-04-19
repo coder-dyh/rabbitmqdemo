1de170b68074ca1a17f098d2054716e1d2ec0d8f
dyh <1374697383@qq.com>
2019-04-19 09:42:42 +0800

    Initial commit

---
+++ rabbitmq-api/src/main/java/com/hmily/rabbitmqapi/common/RabbitMQCommon.java
@@ -0,0 +1,11 @@
+package com.hmily.rabbitmqapi.common;
+
+public interface RabbitMQCommon {
+
+    final static String RABBITMQ_HOST = "47.106.10.32";
+    final static int RABBITMQ_PORT = 5672;
+    final static String RABBITMQ_DEFAULT_VIRTUAL_HOST = "/";
+
+    public final static String RABBITMQ_USERNAME = "guest";
+    public final static String RABBITMQ_PASSWORD = "guest";
+}
